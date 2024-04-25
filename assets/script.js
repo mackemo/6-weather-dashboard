47765c4fed61c09e6f9e6bfb28de2ba2dde79b4a
mackemo <mackenzielmoore14@gmail.com>
2024-04-25 13:47:08 -0400

    made city varibale into value of input, made event listener to fetch data after clicking on search button, created divs for mmain city info

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,34 @@
+const searchBtn = document.getElementById('search-btn');
 
 function searchAPI() {
-    let city = 'Nashville';
-    const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=7f24f20a0a0533ff8f591e0bdf6457c2`;
+    searchBtn.addEventListener('click', function() {
+        let city = document.getElementById('search-input').value;
 
-    fetch(geoURL)
-        .then(function(response) {
-            return response.json();
-        })
+        const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=7f24f20a0a0533ff8f591e0bdf6457c2`;
 
-        .then(function(geoResults) {
-            console.log(geoResults);
-            let lat = geoResults[0].lat;
-            let lon = geoResults[0].lon;
-            console.log(lat, lon);
+        fetch(geoURL)
+            .then(function(response) {
+                return response.json();
+            })
 
-            const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7f24f20a0a0533ff8f591e0bdf6457c2`;
+            .then(function(geoResults) {
+                console.log(geoResults);
+                let lat = geoResults[0].lat;
+                let lon = geoResults[0].lon;
 
-            fetch(weatherURL)
-                .then(function(response) {
-                    return response.json();
-                })
+                const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7f24f20a0a0533ff8f591e0bdf6457c2`;
 
-                .then(function(weatherResults) {
-                    console.log(weatherResults);
-                })
-        }) 
+                fetch(weatherURL)
+                    .then(function(response) {
+                        return response.json();
+                    })
+
+                    .then(function(weatherResults) {
+                        console.log(weatherResults);
+                    })
+            })
+    })
 }
+    
 
 searchAPI();
