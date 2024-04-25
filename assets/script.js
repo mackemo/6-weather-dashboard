b33d212ea9db0aaed99bf69517eabf058aa2803d
mackemo <mackenzielmoore14@gmail.com>
2024-04-25 18:14:15 -0400

    added date to main city header, split on date to only include month, day year

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,8 @@ const searchBtn = document.getElementById('search-btn');
 function searchAPI() {
     searchBtn.addEventListener('click', function() {
         let city = document.getElementById('search-input').value;
-        let mainCity = document.getElementById('main-city');
-        mainCity.textContent = city;
+        // let mainCity = document.getElementById('main-city');
+        // mainCity.textContent = city;
 
         const geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=7f24f20a0a0533ff8f591e0bdf6457c2`;
 
@@ -27,17 +27,21 @@ function searchAPI() {
 
                     .then(function(weatherResults) {
                         console.log(weatherResults);
+                        let dateData = weatherResults.list[0].dt_txt.split(' ')[0];
                         let tempData = weatherResults.list[0].main.temp;
                         let windData = weatherResults.list[0].wind.speed;
                         let humData = weatherResults.list[0].main.humidity;
-                        
+
+                        let cityInfo = document.getElementById('main-city');
                         let temp = document.getElementById('main-temp');
                         let wind = document.getElementById('main-wind');
                         let hum = document.getElementById('main-hum');
 
+                        cityInfo.textContent = city + ' ' + `(${dateData})`;
                         temp.textContent = tempData;
                         wind.textContent = windData;
                         hum.textContent = humData;
+
                     })
             })
     })
